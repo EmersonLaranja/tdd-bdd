9f1ae7a461edef77c184a3fec27fadbfb20a0151
EmersonLaranja <emersonlaranja@gmail.com>
2024-04-15 16:39:22 -0300

    1.4

--- src/adapters/controllers/task/addTask.spec.ts
+++ src/adapters/controllers/task/addTask.spec.ts
@@ -1,6 +1,12 @@
 import { Task } from "../../../entities/task";
-import { AddTask, AddTaskModel } from "../../../usecases";
+import { AddTaskModel, AddTask } from "../../../usecases";
 import { HttpRequest, Validation } from "../../interfaces";
+import { MissingParamError, ServerError } from "../../presentations/api/errors";
+import {
+  badRequest,
+  created,
+  serverError,
+} from "../../presentations/api/httpResponses/httpResponses";
 import { AddTaskController } from "./addTask";
 
 interface SutTypes {
@@ -58,5 +64,41 @@ describe("AddTask Controller", () => {
       description: "any_description",
       date: "any_date",
     });
+  });
+
+  test("Should return 500 if AddTask throws", async () => {
+    const { sut, addTaskStub } = makeSut();
+    jest.spyOn(addTaskStub, "add").mockImplementationOnce(async () => {
+      return new Promise((_, reject) => reject(new Error()));
+    });
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new ServerError()));
+  });
+
+  test("Should return 200 if an valid data is provided", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(created(makeFakeTask()));
+  });
+
+  test("Should call Validation with correct value", async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, "validate");
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test("Should return 400 if an Validation returns an error", async () => {
+    const { sut, validationStub } = makeSut();
+
+    jest
+      .spyOn(validationStub, "validate")
+      .mockReturnValueOnce(new MissingParamError("any_field"));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError("any_field"))
+    );
   });
 });
